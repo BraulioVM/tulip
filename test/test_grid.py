696ff9032ef9f3e34b061ba709c30587a9678eab
Braulio Valdivielso Martínez <brlvldvlsmrtnz@gmail.com>
2015-12-29 00:57:19 +0100

    Improved tests for tulip.grid

--- test/test_grid.py
+++ test/test_grid.py
@@ -1,6 +1,7 @@
 from tulip.util import get_average_color, coordinates
 from tulip.grid import Grid
 from PIL import Image
+import os
 
 def test_basic_grid():
     size = 100, 100
@@ -12,3 +13,30 @@ def test_basic_grid():
     grid = Grid(image, width=4, height=4)
     
     assert grid[0, 0] == get_average_color(image, width=25, height=25)
+    assert grid[1, 0] == get_average_color(image, x=25, y=0, width=25, height=25)
+
+    assert grid[0, 1] == get_average_color(image, x=0, y=25, width=25, height=25)
+
+    assert grid[3, 3] == get_average_color(image, x=75, y=75, width=25, height=25)
+
+
+
+
+def test_from_file():
+    size = 100, 100
+    filename = 'test.png'
+    image = Image.new('RGB', size)
+    
+    for i, j in coordinates(size):
+        image.putpixel((i, j), (i, j + 3, i + j))
+
+    try:
+        image.save(filename)
+        grid = Grid.from_file(filename, width=2, height=2)
+        assert grid[0, 0] == get_average_color(image, width=50, height=50)
+
+        assert grid[1, 1] == get_average_color(image, x=50, y=50)
+        
+    finally:
+        os.remove(filename)
+        
