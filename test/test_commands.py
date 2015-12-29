e16170de6270b5071f4e7fdf7b6ee16c5acb5a9e
Braulio Valdivielso Martínez <brlvldvlsmrtnz@gmail.com>
2015-12-29 17:28:05 +0100

    Added basic test for the tulip CLI utility

---
+++ test/test_commands.py
@@ -0,0 +1,23 @@
+import click
+import os
+from click.testing import CliRunner
+from tulip import tulip
+
+here = os.path.abspath(os.path.dirname(__file__))
+test_resources_path = os.path.join(here, 'test-resources')
+
+get_test_resource_path = lambda s: os.path.join(test_resources_path, s)
+
+def test_basic_invocation():
+    height, width = 39, 136
+    
+    runner = CliRunner()
+
+    result = runner.invoke(tulip, ['--width', str(width), '--height', str(height), get_test_resource_path('batman.png')])
+
+    assert result.exit_code == 0
+    
+    with open(get_test_resource_path('batman-output.txt')) as f:
+        assert f.read() == result.output
+
+        
