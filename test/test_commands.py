cecce45fdab3bddfe5f9511913a42f2b4accf285
Braulio Valdivielso Martínez <brlvldvlsmrtnz@gmail.com>
2015-12-30 14:50:50 +0100

    Added a test for the new feature

--- test/test_commands.py
+++ test/test_commands.py
@@ -13,11 +13,29 @@ def test_ascii_invocation():
     
     runner = CliRunner()
 
-    result = runner.invoke(tulip, ['--width', str(width), '--height', str(height), '--just-ascii', get_test_resource_path('batman.png')])
+    result = runner.invoke(tulip, [
+        '--width', str(width), 
+        '--height', str(height), 
+        '--just-ascii', 
+        get_test_resource_path('batman.png')
+    ])
 
     assert result.exit_code == 0
     
-    with open(get_test_resource_path('batman-output.txt')) as f:
+    with open(get_test_resource_path('batman-ascii-output.txt')) as f:
         assert f.read() == result.output
 
         
+def test_normal_invocation():
+    height, width = 24, 80
+    runner = CliRunner()
+
+    result = runner.invoke(tulip, [
+        '--width', str(width),
+        '--height', str(height),
+        get_test_resource_path('batman.png')
+    ])
+
+    assert result.exit_code == 0
+    with open(get_test_resource_path('batman-output.txt')) as f:
+        assert f.read() == result.output
